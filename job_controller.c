e2bb9252ea4122ace4823cda453148070843b0ed
NikhilKumarPandiri <nikhil22@uab.edu>
2022-04-17 20:34:48 -0500

    contiuation for the first part, written functions for job scheduler, submiting the jobs, show jobs and job controller

--- job_controller.c
+++ job_controller.c
@@ -122,3 +122,128 @@ void startJob(JOB* job, int index)
 		pthread_mutex_unlock(&runningJobsMutex);
 	}
 }
+//the main scheduler function which periodically checks the running jobs and removes finished jobs
+void* scheduler(void* arg)
+{
+	// check if any of the running jobs have finished
+	// if finished the clear up the core to add new job
+	while (1)
+	{
+		int stop = stopScheduler && isJobQueueEmpty();
+
+		pthread_mutex_lock(&runningJobsMutex);
+
+		//printf("checking index : [%d]\n", runningJobs->runningJobsIndex);
+
+		for (int i = 0; i < runningJobs->maxJobs; i++)
+		{
+			int status;
+			int current_pid = runningJobs->runningJobsPid[i];
+			//printf("current_pid = [%d]\n",current_pid);
+			if (current_pid <= 0)
+				continue;
+			pid_t return_pid = waitpid(current_pid, &status, WNOHANG); /* WNOHANG def'd in wait.h */
+			if (return_pid == -1) {
+				printf("Unable to know status of pid %d\n", current_pid);
+			}
+			else if (return_pid == 0) {
+				if (stop)
+					i--;
+
+				continue;
+			}
+			else if (return_pid == current_pid) {
+				runningJobs->runningJobsIndex |= (1 << i);
+				runningJobs->runningJobsPid[i] = 0;
+				freeUpJob(runningJobs->runningJobs[i]);
+				runningJobs->runningJobs[i] = NULL;
+			}
+			if (runningJobs->runningJobs[i] == NULL)
+			{
+				runningJobs->runningJobsIndex |= (1 << i);
+			}
+		}
+		pthread_mutex_unlock(&runningJobsMutex);
+
+		if (stop)
+			break;
+
+		int freeIndex = 0;
+		while (!isJobQueueEmpty() && (freeIndex = isSchedulerFree()) != 0)
+		{
+			//check for jobs from queue
+			JOB* job = getNextJobInQueue();
+			//printf("Starting new job with job id : [%d]\n\n", job->job_id);
+
+			startJob(job, freeIndex - 1);
+		}
+		sleep(1);
+	}
+	return NULL;
+}
+
+void submitJob(char* jobName)
+{
+	JOB* newJob = createNewJob(jobName);
+	addToJobQueue(newJob);
+}
+
+void showJobs()
+{
+
+	printf("JOBID\tJOB NAME\t\t\tSTATUS\n");
+	printf("--------------------------------------------------------------------------------------------------\n");
+	pthread_mutex_lock(&runningJobsMutex);
+	for (int i = 0; i < runningJobs->maxJobs; i++)
+	{
+		if (runningJobs->runningJobs[i] != NULL)
+		{
+			printf("%d\t%s\t\t\t%s\n", runningJobs->runningJobs[i]->job_id, runningJobs->runningJobs[i]->job_name, runningJobs->runningJobs[i]->status == WAITING ? "WAITING" : "RUNNING");
+		}
+	}
+	pthread_mutex_unlock(&runningJobsMutex);
+
+	pthread_mutex_lock(&jobQueueMutex);
+	JQ_NODE* tempNode = jq->head;
+	for (; tempNode != NULL; tempNode = tempNode->next)
+	{
+		printf("%d\t%s\t\t\t%s\n", tempNode->job->job_id, tempNode->job->job_name, tempNode->job->status == WAITING ? "WAITING" : "RUNNING");
+	}
+	pthread_mutex_unlock(&jobQueueMutex);
+}
+
+// main function to initialize entire job controller
+void initializeJobController(int maxJobs)
+{
+
+	// initialize the runningJobs 
+	pthread_mutex_init(&runningJobsMutex, NULL);
+
+	runningJobs = malloc(sizeof(RUNNING_JOBS));
+	runningJobs->maxJobs = maxJobs;
+	runningJobs->runningJobs = malloc(sizeof(JOB*) * maxJobs);
+	runningJobs->runningJobsPid = malloc(sizeof(pid_t) * maxJobs);
+	memset(runningJobs->runningJobsPid, 0, sizeof(pid_t) * maxJobs);
+
+	runningJobs->runningJobsIndex = (int)pow(2, maxJobs) - 1;
+
+	//initialize the jobQueue
+	pthread_mutex_init(&jobQueueMutex, NULL);
+	jq = malloc(sizeof(JQ));
+	jq->head = NULL;
+	jq->tail = NULL;
+}
+//call at end of scheduling
+void freeJobController()
+{
+	for (int i = 0; i < runningJobs->maxJobs; i++)
+	{
+		if (runningJobs->runningJobs[i] != NULL)
+			freeUpJob(runningJobs->runningJobs[i]);
+	}
+	free(runningJobs->runningJobsPid);
+	free(runningJobs->runningJobs);
+	free(runningJobs);
+	free(jq);
+}
+
